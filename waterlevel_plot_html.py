d8a44f903ce9f74c438db2fcba24102bfcc57e41
Nicholas Esterer <nicholas.esterer@gmail.com>
2020-03-30 12:05:08 -0400

    We sidestep the unequally spaced date problem
    We just interpolate the times. This is better too because you can reduce
    the amount of data (say plot the water level every hour).

--- waterlevel_plot_html.py
+++ waterlevel_plot_html.py
@@ -3,6 +3,10 @@ import sys
 import dbm
 import plotly.graph_objects as go
 import time
+import numpy as np
+
+# 60 minutes between observations
+TIME_IVAL=3600
 
 times=[]
 levels=[]
@@ -13,22 +17,29 @@ with dbm.open('data/waterlevel_history','r') as db:
         entries=[b.decode() for b in db[k].split(bytes(',',encoding='utf-8'))]
         nv=float(entries[-1])
         dt=' '.join(entries[:-1])
-        times.append(time.strftime('%Y %m %d %H:%M',time.localtime(nk)))
+        times.append(nk)
         levels.append(nv)
         date_time.append(dt)
 
+# Because Plotly seems to assume dates are equally spaced, we linearly
+# interpolate the depths looked up at equally spaced times
+t=np.array(times)
+l=np.array(levels)
+t_=np.arange(t.min(),t.max()+TIME_IVAL,TIME_IVAL)
+l_=np.interp(t_, t, l)
+
+# now convert the equally spaced times to date formats
+dt_=[time.strftime('%Y %m %d %H:%M',time.localtime(t__)) for t__ in t_]
+
 fig = go.Figure(
-    data=[go.Line(x=times,y=levels)]
+    data=[go.Line(x=dt_,y=l_)]
 )
 
-#fig.update_layout(
-#    xaxis=dict(
-#        #tickmode='array',
-#        #tickvals=times[::len(times)//10],
-#        #ticktext=date_time[::len(times)//10],
-#        tickformat='%x'
-#    )
-#)
+fig.update_layout(
+    title='St. Lawrence water level measured at Jetee #1',
+    xaxis=dict(title='Date'),
+    yaxis=dict(title='Height (metres)')
+)
 
 #fig.write_html(sys.stdout)
 fig.show()
